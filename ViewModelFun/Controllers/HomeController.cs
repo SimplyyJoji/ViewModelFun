e7032cf32134fbd93903b5fe6a692eadfd964b29
SimplyyJoji <59666562+SimplyyJoji@users.noreply.github.com>
2021-08-07 17:49:22 -0700

    Added Controllers

---
+++ ViewModelFun/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+using YourNamespace.Models;
+
+namespace HelloASP
+{
+  public class HomeController : Controller
+{
+
+   [HttpGet("")]
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpGet("user")]
+    public IActionResult UserPage()
+    {
+      User someUser = new User()
+      {
+        FirstName = "Bill",
+        LastName = "TestBill"
+      };
+
+      return View(someUser);
+    }
+
+
+
+    [HttpGet("users")]
+    public IActionResult Users()
+    {
+        // to a View that has defined a model as @model string[]
+        string[] names = new string[]
+        {
+            "Sally",
+            "Billy",
+            "Joey",
+            "Moose"
+        };
+        return View(names);
+    }
+
+  public IActionResult Numbers()
+    {
+        // to a View that has defined a model as @model string[]
+        int[] numbers = new int[]
+        {
+            1,2,3,4,5,6,7,8,9
+        };
+        return View(numbers);
+    }
+
+
+}
+
+
+
+}
+
+
+
+
+
+    
